860d2c934b8fae691bc0bc18f8f8366f8964eb54
aalexeev239 <alexeev.andrey.a@gmail.com>
2014-05-15 12:54:47 +0400

    Скроллинг
    Правильное расположение, анимация

--- _public/js/jquery_aalexeev.js
+++ _public/js/jquery_aalexeev.js
@@ -1,6 +1,22 @@
 // Андрей Алексеев [AA]
 // alexeev.andrey.a@gmail.com
 
-$(document).ready(function{
+$(document).ready(function(){
+	//bootstrap scrollspy [AA]
 	$('body').scrollspy({ target: '.main-nav' });
+
+	//smooth scrolling [AA]
+	$('a[href*=#]:not([href=#])').click(function() {
+		console.log("clicked!");
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 600);
+        return false;
+      }
+    }
+  });
 });
